48c62bc14dc4d9539b7934b5ce8db55795494b58
steven <theyoung2002@naver.com>
2021-04-19 02:25:40 +0900

    update

--- php-redis/run_docker.sh
+++ php-redis/run_docker.sh
@@ -1,1 +1,13 @@
 docker build --tag stevenna/guestbook .
+
+dockerpath="stevenna/guestbook"
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+# docker login
+echo "user --> $DOCKER_USER"
+docker tag stevenna/guestbook:latest $dockerpath:latest
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath:latest
